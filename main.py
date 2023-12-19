3e3df94e4124e834a76c04440eaf284d6bda0aef
codinexpress <72963187+codinexpress@users.noreply.github.com>
2023-12-19 13:47:53 +0800

    Update main.py
    made main.py more to into the bots administration and management, instead of more command handler

--- main.py
+++ main.py
@@ -1,63 +1,61 @@
-import os
 import discord
-from discord.ext import commands
-from datetime import datetime
-import wikipedia
+import logging
+from logging.handlers import RotatingFileHandler
+import os
+from dotenv import load_dotenv
 
+# Load environment variables from the .env file
+load_dotenv()
 
-DISCORD_TOKEN = os.getenv("discord_token")
+# Assign the path to the Python executable
+python_executable = "/usr/local/bin/python"
 
-# Create an auto-sharded bot instance with a command prefix and 5 shards
+# Create the "logs" directory if it doesn't exist
+log_dir = "logs"
+os.makedirs(log_dir, exist_ok=True)
+
+# Create a rotating file handler for the bot's logger
+handler = RotatingFileHandler(
+    filename=os.path.join(log_dir, "panhs_bot.log"),
+    encoding='utf-8',
+    maxBytes=12 * 1024 * 1024,  # 12 MiB
+    backupCount=8,  # Rotate through 8 files
+)
+
+# Add the handler to the logger
+logger = logging.getLogger('panhs_bot')
+logger.addHandler(handler)
+
+# Create an instance of the auto-sharded bot
 intents = discord.Intents.all()
-bot = commands.AutoShardedBot(command_prefix='!', shards=5, intents=intents)
+bot = discord.AutoShardedClient(intents=intents)
 
+# Function to load all cogs listed in cogs.txt
+def load_cogs():
+    cog_dir = "cogs"
+    
+    with open("cogs.txt", "r") as file:
+        cogs = [line.strip() for line in file.readlines()]
+
+    for cog in cogs:
+        cog_path = os.path.join(cog_dir, cog + ".py")
+        try:
+            bot.load_extension(cog_path)
+            logger.info(f"Cog '{cog}' loaded successfully.")
+        except Exception as e:
+            logger.error(f"Error loading cog '{cog}': {e}")
+
+# Load all cogs before the bot starts
+load_cogs()
+
+# Event triggered when the bot is ready
 @bot.event
 async def on_ready():
-    """Event triggered when the bot is ready."""
-    print(f'{bot.user} has connected to Discord!')
+    logger.info(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
+    logger.info('------')
 
-@bot.command(name='hello', aliases=['hi'], help='Greet the user')
-async def hello(ctx: commands.Context):
-    """Command to greet the user."""
-    await ctx.send(f'Hello {ctx.author.name}!')
+# Get the bot token from the environment variable
+bot_token = os.getenv("DISCORD_TOKEN")
 
-@bot.command(name='botinfo', aliases=['info'], help='Display bot information')
-async def botinfo(ctx: commands.Context):
-    """Command to display bot information."""
-    bot_info = f'Bot Name: {bot.user.name}\nBot ID: {bot.user.id}\nNumber of Shards: {bot.shard_count}'
-    await ctx.send(bot_info)
-
-@bot.command(name='wikipedia', aliases=['wiki'], help='Get a summary from Wikipedia')
-async def wikipedia_summary(ctx: commands.Context, *, query: str):
-    """Command to get a Wikipedia summary for a given query."""
-    try:
-        summary = wikipedia.summary(query, sentences=3)
-        await ctx.send(f'**Wikipedia Summary for {query}:**\n{summary}')
-    except wikipedia.exceptions.DisambiguationError as e:
-        await ctx.send(f'Multiple results found. Please be more specific.\nOptions: {", ".join(e.options)}')
-    except wikipedia.exceptions.PageError:
-        await ctx.send(f'No results found for {query}.')
-
-@bot.command(name='serverinfo', aliases=['sinfo'], help='Display server information')
-async def serverinfo(ctx: commands.Context):
-    """Command to display information about the server using an embed."""
-    server = ctx.guild
-    embed = discord.Embed(title=f'Server Information - {server.name}', color=discord.Color.blue())
-    embed.add_field(name='Server ID', value=server.id, inline=False)
-    embed.add_field(name='Members', value=server.member_count, inline=False)
-    embed.add_field(name='Owner', value=server.owner.name, inline=False)
-    embed.add_field(name='Created At', value=server.created_at.strftime('%Y-%m-%d %H:%M:%S'), inline=False)
-    await ctx.send(embed=embed)
-
-@bot.command(name='userinfo', aliases=['uinfo'], help='Display user information')
-async def userinfo(ctx: commands.Context, user: discord.Member = None):
-    """Command to display information about a user using an embed."""
-    user = user or ctx.author
-    embed = discord.Embed(title=f'User Information - {user.name}', color=discord.Color.green())
-    embed.set_thumbnail(url=user.avatar.url)
-    embed.add_field(name='User ID', value=user.id, inline=False)
-    embed.add_field(name='Joined Server', value=user.joined_at.strftime('%Y-%m-%d %H:%M:%S'), inline=False)
-    embed.add_field(name='Joined Discord', value=user.created_at.strftime('%Y-%m-%d %H:%M:%S'), inline=False)
-    await ctx.send(embed=embed)
-
-bot.run(DISCORD_TOKEN)
+# Run the bot
+bot.run(bot_token)
